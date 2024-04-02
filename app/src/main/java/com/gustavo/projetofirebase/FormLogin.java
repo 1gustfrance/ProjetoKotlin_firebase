ea6ef94384537d2a28ebd7a59e40027b614760f3
Gustavo de França Cabral <gstv643@gmail.com>
2024-04-02 15:13:34 -0300

    recuperando dados de perfil

--- app/src/main/java/com/gustavo/projetofirebase/FormLogin.java
+++ app/src/main/java/com/gustavo/projetofirebase/FormLogin.java
@@ -22,6 +22,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class FormLogin extends AppCompatActivity {
 
@@ -102,6 +103,17 @@ public class FormLogin extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser usuarioAtual = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(usuarioAtual != null){
+            TelaPrincipal();
+        }
     }
 
     private void TelaPrincipal(){
